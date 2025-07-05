e0a3244e4b7ca61768f5aedf8e75f97a2a5a94dc
John McGlone <john.mcglone@rumble.com>
2025-07-05 15:57:08 -0400

    More public func

--- Sources/EntityStore/EntityStore.swift
+++ Sources/EntityStore/EntityStore.swift
@@ -2,29 +2,29 @@ import SwiftUI
 
 @MainActor
 public final class EntityStore {
-    static let shared = EntityStore()
+  static public let shared = EntityStore()
 
-    private var boxes: [ObjectIdentifier: [AnyHashable: AnyObject]] = [:]
+  private var boxes: [ObjectIdentifier: [AnyHashable: AnyObject]] = [:]
 
-    func entity<T: Identifiable & Equatable & Hashable>(for model: T) -> EntityBox<T> {
-        let typeID = ObjectIdentifier(T.self)
-        let id = model.id
+  public func entity<T: Identifiable & Equatable & Hashable>(for model: T) -> EntityBox<T> {
+    let typeID = ObjectIdentifier(T.self)
+    let id = model.id
 
-        if let box = boxes[typeID]?[id] as? EntityBox<T> {
-            box.value = model // update value if it changed
-            return box
-        } else {
-            let box = EntityBox(model)
-            if boxes[typeID] == nil {
-                boxes[typeID] = [:]
-            }
-            boxes[typeID]?[id] = box
-            return box
-        }
+    if let box = boxes[typeID]?[id] as? EntityBox<T> {
+      box.value = model // update value if it changed
+      return box
+    } else {
+      let box = EntityBox(model)
+      if boxes[typeID] == nil {
+        boxes[typeID] = [:]
+      }
+      boxes[typeID]?[id] = box
+      return box
     }
+  }
 
-    func entity<T: Identifiable & Equatable & Hashable>(for id: T.ID) -> EntityBox<T>? {
-        let typeID = ObjectIdentifier(T.self)
-        return boxes[typeID]?[id] as? EntityBox<T>
-    }
+  public func entity<T: Identifiable & Equatable & Hashable>(for id: T.ID) -> EntityBox<T>? {
+    let typeID = ObjectIdentifier(T.self)
+    return boxes[typeID]?[id] as? EntityBox<T>
+  }
 }
