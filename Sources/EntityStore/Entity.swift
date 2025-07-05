74a3987f221947ec7e4e00c18af5594566141816
John McGlone <john.mcglone@rumble.com>
2025-07-05 15:50:28 -0400

    Made property wrapper public

--- Sources/EntityStore/Entity.swift
+++ Sources/EntityStore/Entity.swift
@@ -2,20 +2,20 @@ import SwiftUI
 
 @MainActor
 @propertyWrapper
-struct Entity<T: Identifiable & Equatable & Hashable>: DynamicProperty {
-    @StateObject private var box: EntityBox<T>
+public struct Entity<T: Identifiable & Equatable & Hashable>: DynamicProperty {
+  @StateObject private var box: EntityBox<T>
 
-    var wrappedValue: T {
-        get { box.value }
-        nonmutating set { box.value = newValue }
-    }
+  public var wrappedValue: T {
+    get { box.value }
+    nonmutating set { box.value = newValue }
+  }
 
-    var projectedValue: EntityBox<T> {
-        box
-    }
+  public var projectedValue: EntityBox<T> {
+    box
+  }
 
-    init(wrappedValue: T) {
-        let entity = EntityStore.shared.entity(for: wrappedValue)
-        _box = StateObject(wrappedValue: entity)
-    }
+  public init(wrappedValue: T) {
+    let entity = EntityStore.shared.entity(for: wrappedValue)
+    _box = StateObject(wrappedValue: entity)
+  }
 }
